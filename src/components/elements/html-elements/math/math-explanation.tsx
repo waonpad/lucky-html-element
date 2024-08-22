950aefeedadd94fbd3d63169aacff05a58aa310a
asai <asai@fenrir.co.jp>
2024-08-22 11:17:16 +0900

    途中

--- src/components/elements/html-elements/math/math-explanation.tsx
+++ src/components/elements/html-elements/math/math-explanation.tsx
@@ -1,9 +1,24 @@
 import { ExplanationContainer } from "@/components/elements/explanation-container";
+import { ExplanationMemoContainer } from "../../explanation-memo-container";
+import { ExternalLink } from "../../external-link";
 
 export const MathExplanation = () => {
   return (
     <ExplanationContainer>
-      <div />
+      <ExplanationMemoContainer>
+        <li>
+          <p>
+            最上位の
+            <ExternalLink href="https://developer.mozilla.org/ja/docs/Web/MathML">MathML要素</ExternalLink>
+          </p>
+        </li>
+        <li>
+          <p>数式を記述するために使う</p>
+        </li>
+        <li>
+          <p>使うことになったら調べる</p>
+        </li>
+      </ExplanationMemoContainer>
     </ExplanationContainer>
   );
 };
--- src/components/elements/html-elements/svg/svg-explanation.tsx
+++ src/components/elements/html-elements/svg/svg-explanation.tsx
@@ -1,9 +1,36 @@
 import { ExplanationContainer } from "@/components/elements/explanation-container";
+import { ExplanationExampleContainer } from "../../explanation-example-container";
+import { ExplanationMemoContainer } from "../../explanation-memo-container";
+import { ExternalLink } from "../../external-link";
 
 export const SvgExplanation = () => {
   return (
     <ExplanationContainer>
-      <div />
+      <ExplanationMemoContainer>
+        <li>
+          <p>入れ子可能</p>
+        </li>
+        <li>
+          <p>
+            <ExternalLink href="https://developer.mozilla.org/ja/docs/Web/SVG/Element">
+              SVG 要素リファレンス
+            </ExternalLink>
+          </p>
+        </li>
+      </ExplanationMemoContainer>
+      <ExplanationExampleContainer
+        examples={[
+          {
+            title: "単純な例",
+            element: (
+              <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                <title>サンプルSVG</title>
+                <circle cx="50" cy="50" r="40" stroke="black" strokeWidth="3" fill="red" />
+              </svg>
+            ),
+          },
+        ]}
+      />
     </ExplanationContainer>
   );
 };
