5f8e4b1f70f4b5b821f309aa6aa3f4a018c44c55
asai <asai@fenrir.co.jp>
2024-08-25 04:41:25 +0900

    はば

--- src/components/elements/content-container.tsx
+++ src/components/elements/content-container.tsx
@@ -5,5 +5,9 @@ type Props = {
 };
 
 export const ContentContainer = ({ children }: Props) => {
-  return <div className="my-8 w-3/4 space-y-6 rounded-md border-gray-300 bg-slate-50 p-4 shadow-lg">{children}</div>;
+  return (
+    <div className="my-8 w-11/12 space-y-6 rounded-md border-gray-300 bg-slate-50 p-4 shadow-lg md:w-5/6 lg:w-3/4">
+      {children}
+    </div>
+  );
 };
